1f5349be7c9075331e575faff895afdc1205e9a6
Ahmad Rehan <ahmedrehan98@gmail.com>
2024-05-23 08:04:05 +0300

    Added ui for edit and delete categories. functionality still unimplemented

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -21,5 +21,60 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("Name", "Test is an invalid value.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null or 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category? categoryFromDb = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+            if (categoryFromDb is null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("Name", "Test is an invalid value.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
     }
 }
